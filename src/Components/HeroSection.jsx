64015a23f6e7ea4458f842c1d62587aeffc3a290
Soumajyoti <soumyabwn3@gmail.com>
2024-03-04 15:46:04 +0530

    responsiveness added

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -14,6 +14,7 @@ import watch from '../Images/watch.jpg'
 import camera from '../Images/camera.jpg'
 import bluetoothspeaker from '../Images/bluetoothspeaker.jpg'
 import phone from '../Images/phone.jpg'
+import mega_sale_deals from '../Images/mega_sale_deals.png'
 
 export const HeroSection = () => {
     return (
@@ -25,6 +26,9 @@ export const HeroSection = () => {
                 <h1>Crazy</h1>
                 <br />
                 <h4>DEALS</h4>
+            </div>
+            <div className="crazy_deal_image">
+                <img src={mega_sale_deals} alt="" />
             </div>
             {/* Shop-section */}
             <h2>Fashion</h2>
@@ -148,18 +152,18 @@ export const HeroSection = () => {
                         <a href='https://www.sceceaot.com/projects'>Investor Relations</a>
                         <a href='https://www.sceceaot.com/projects'>Shop-Topia Devices</a>
                         <a href='https://www.sceceaot.com/projects'>Connect With Us:
-                            <div className='social-handle'> 
-                            <svg className='social-icons' fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                                viewBox="0 0 24 24">
-                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                            </svg>
-                            <svg className='social-icons' fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"  viewBox="0 0 24 24">
-                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                            </svg>
-                            <svg className='social-icons' fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"  viewBox="0 0 24 24">
-                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                            </svg>
+                            <div className='social-handle'>
+                                <svg className='social-icons' fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    viewBox="0 0 24 24">
+                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                </svg>
+                                <svg className='social-icons' fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                </svg>
+                                <svg className='social-icons' fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                                </svg>
                             </div>
                         </a>
                     </ul>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,60 +10,59 @@ import truck from '../Images/truck.gif'
 export const Navbar = () => {
     return (
         <>
-       <header> 
-        <div className="navbar">
+            <header>
+                <div className="navbar">
 
-        <div className="nav-logo border">
-            <div className="logo">
-            <img className='logo-img' src={logo} alt="cart"/>
-            <p className='web-name'>Shop-Topia</p>
-            </div>
-        </div>
+                    <div className="nav-logo border">
+                        <div className="logo">
+                            <img className='logo-img' src={logo} alt="cart" />
+                            <p className='web-name'>Shop-Topia</p>
+                        </div>
+                    </div>
 
-        <div className="nav-search">
-            <select className="search-select"> 
-                <option>All</option>
-            </select>
-            <input placeholder="Search Products" className="search-input"/>
-            <div className="search-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-            </div>
-        </div>
+                    <div className="nav-search">
+                        <select className="search-select">
+                            <option>All</option>
+                        </select>
+                        <input placeholder="Search Products" className="search-input" />
+                        <div className="search-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+                        </div>
+                    </div>
 
-        <div className="nav-signin border">
-            {/* <p><span>Hello, Sign in</span></p> */}
-            <img className='profile-icon' src={profile} alt="Profile Icon"/>
-            <p className='navcmn'>Profile</p>
-        </div>
+                    <div className="nav-signin border">
+                        {/* <p><span>Hello, Sign in</span></p> */}
+                        <img className='profile-icon' src={profile} alt="Profile Icon" />
+                        <p className='navcmn'>Profile</p>
+                    </div>
 
-        <div className="nav-return border">
-            <img className='truck-img' src={truck} alt="cart"/>
-            <p className="nav-second navcmn">Returns & Orders</p>
-        </div>
-        
-        <div className="nav-cart border">
-        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg> */}
-        <img src={trolley} alt="cart"/>
-            <p className='navcmn'>Cart</p>
-        </div>
-    </div >
-    <div className="panel">
-        <div className="panel-all">
-            <i className="fa-solid fa-bars"></i>
-            All
-        </div>
-        <div className="panel-ops">
-            <p>Today's Deals</p>
-            <p>Customer Service</p>
-            <p>Registry</p>
-            <p>Gift Cards</p>
-            <p>Sell</p>
-        </div>
-        <div className="panel-deals">
-            <p>Shop deal in Electronics</p>
-        </div>
-    </div>
-</header>
+                    <div className="nav-return border">
+                        <img className='truck-img' src={truck} alt="cart" />
+                        <p className="nav-second navcmn">Returns & Orders</p>
+                    </div>
+
+                    <div className="nav-cart border">
+                        <img src={trolley} alt="cart" />
+                        <p className='navcmn'>Cart</p>
+                    </div>
+                </div >
+                <div className="panel">
+                    {/* <div className="panel-all">
+                        <p>All</p>
+
+                    </div> */}
+                    <div className="panel-ops">
+                        <p>Today's Deals</p>
+                        <p>Customer Service</p>
+                        <p>Registry</p>
+                        <p>Gift Cards</p>
+                        <p>Sell</p>
+                    </div>
+                    <div className="panel-deals">
+                        <p>Shop deal in Electronics</p>
+                    </div>
+                </div>
+            </header>
         </>
     )
 }
